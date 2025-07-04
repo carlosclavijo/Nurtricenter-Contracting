678e2a749fcda916c4e51bd83e407327946b82d8
Carlos Clavijo <carlo96cf@gmail.com>
2025-07-04 19:20:46 -0400

    sonarcloud settings name correction -testing

--- start-sonarcloud.sh
+++ start-sonarcloud.sh
@@ -18,15 +18,15 @@ PROJECT_VERSION=$2
 
 echo "### Beginning SonarScanner..."
 
-dotnet sonarscanner begin \
+.sonar/scanner/dotnet-sonarscanner begin \
   /k:"carlosclavijo_Nurtricenter-Contracting" \
   /o:"carlosclavijo" \
   /d:sonar.token="$SONAR_TOKEN" \
-    /v:"$PROJECT_VERSION" \
-    /d:sonar.host.url="https://sonarcloud.io" \
-    /d:sonar.cs.opencover.reportsPaths="**/*/coverage.cobertura.xml" \
-    /d:sonar.cs.vstest.reportsPaths="**/*/*.trx" \
-    /d:sonar.exclusions="**/Migrations/**/*.cs"
+  /v:"$PROJECT_VERSION" \
+  /d:sonar.host.url="https://sonarcloud.io" \
+  /d:sonar.cs.opencover.reportsPaths="**/*/coverage.cobertura.xml" \
+  /d:sonar.cs.vstest.reportsPaths="**/*/*.trx" \
+  /d:sonar.exclusions="**/Migrations/**/*.cs"
 
 echo "### Building solution..."
 dotnet build Contracting.sln
@@ -37,16 +37,15 @@ dotnet test Contracting.sln \
   --results-directory TestResults \
   --logger "trx;LogFileName=test-results.trx"
 
-
 echo "### Copying coverage report..."
-COVERAGE_FILE=$(find . -path '*/coverage.opencover.xml' | head -n 1)
+COVERAGE_FILE=$(find . -name "coverage.cobertura.xml" | head -n 1)
 
 if [ -f "$COVERAGE_FILE" ]; then
-  cp "$COVERAGE_FILE" ./coverage.opencover.xml
+  cp "$COVERAGE_FILE" ./coverage.cobertura.xml
 else
-  echo "❌ coverage.opencover.xml not found"
+  echo "❌ coverage.cobertura.xml not found"
   exit 1
 fi
 
 echo "### Finishing SonarScanner..."
-dotnet sonarscanner end /d:sonar.token="$SONAR_TOKEN"
+.sonar/scanner/dotnet-sonarscanner end /d:sonar.token="$SONAR_TOKEN"
