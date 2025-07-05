853f77d2f48d2094a1d5e285fffa936451d80069
Carlos Clavijo <carlo96cf@gmail.com>
2025-07-04 21:12:40 -0400

    Healthcheck

---
+++ Contracting.WebApi/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NutritionalAdvice.WebApi.Controllers
+{
+	[ApiController]
+	[Route("health")]
+	public class HealthController : ControllerBase
+	{
+		[HttpGet]
+		public IActionResult GetHealth()
+		{
+			return Ok(new { status = "ok" });
+		}
+	}
+}
